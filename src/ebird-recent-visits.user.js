e7481a95468570e185665235bc92e95595082116
Stuart MacKay <smackay@flagstonesoftware.com>
2023-10-30 09:20:43 +0000

    Bump version number for Recent Visits script
    The version number is used when the script is hosted on repositories
    such as greasyfork or openuserjs.

--- src/ebird-recent-visits.user.js
+++ src/ebird-recent-visits.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     eBird Recent Visits
-// @version  2.0.1
+// @version  2.1.0
 // @description Various fixes to make the Recent Visits page usable.
 // @include  https://ebird.org/region/*/activity*
 // @include  https://ebird.org/hotspot/*/activity*
